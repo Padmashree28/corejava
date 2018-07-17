a6ae87e9d62ea90ed6c86d2e0372b82976413d42
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-17 20:29:02 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,14 @@
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=11;
+		do {
+			System.out.println("num is even");
+			break;
+		}while(i%2==0);
+
+	}
+
+}
