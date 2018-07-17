546c8b8d6406c960e28080225ed82e98d8636df8
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-17 20:44:56 +0530

    Add files via upload

---
+++ Asciicode.java
@@ -0,0 +1,16 @@
+
+public class Asciicode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num[]= {77,65,78,71,79};
+		String str=null;
+		for(int i:num) {
+			str=Character.toString((char)i);
+			System.out.println(str);
+		}
+		
+
+	}
+
+}
