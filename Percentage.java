c09ce0dafbf35339c9c1fbaab5c24950df75ceba
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-17 20:30:10 +0530

    Add files via upload

---
+++ Percentage.java
@@ -0,0 +1,17 @@
+
+public class Percentage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double m1,m2,m3;
+		m1=40.2;
+		m2=50.1;
+		m3=70.3;
+		double total=m1+m2+m3;
+		double avg=(m1+m2+m3)/3;
+		double percentage=avg*100;
+		System.out.println("percentage is:" +percentage);
+
+	}
+
+}
