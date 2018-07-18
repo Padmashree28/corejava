66b994ee5b672ee5bbfed522aa4ec8ac8a52f9eb
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-18 13:38:36 +0530

    Add files via upload

---
+++ TestStudent.java
@@ -0,0 +1,16 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		s.m1=50;
+		s.m2=49;
+		s.m3=100;
+		s.grade();
+		s.result();
+		s.total();
+
+	}
+
+}
