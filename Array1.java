2d0ec95ef44c7623b93c2f58c9cf1bb98a0842db
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-18 13:29:43 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,18 @@
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int arr[]= {23,34,45,56,67};
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println("\n");
+		for(int i=arr.length-1;i>=0;i--) {
+			System.out.println(arr[i]);
+
+	}
+
+}
+}
