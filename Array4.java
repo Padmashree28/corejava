2768030e6abfc25b920cbe79098ed4751c2cee3a
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 17:09:54 +0530

    Add files via upload

---
+++ Array4.java
@@ -0,0 +1,21 @@
+
+public class Array4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]= {2,3,4,5};
+int sum=0;
+for(int i=0;i<arr.length;i++) {
+	if(arr[i]%2==0) 
+	{
+	sum=sum+arr[i];
+
+	
+}
+	}
+System.out.println("sum is:" +sum);
+	}
+}
+
+
+
