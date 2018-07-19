448f2310b5551285606612955873580b5c91ab48
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 19:44:58 +0530

    Add files via upload

---
+++ Arrayelmnt.java
@@ -0,0 +1,24 @@
+
+public class Arrayelmnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {12,23,34,26,45};
+		for (int i=0;i<arr.length;i++)
+		{
+			if (arr[i]==26)
+			{
+				System.out.println("found the number 26");
+				break;
+			}
+			else
+			{
+				System.out.println("not found");
+			}
+			
+		}
+
+
+	}
+
+}
