67fdeaef8f2d6df01e093d0c9907a71a0a9076e0
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 13:34:49 +0530

    Add files via upload

---
+++ Commandlinearg.java
@@ -0,0 +1,12 @@
+
+public class Commandlinearg {
+
+	public static void main(String[] args) {
+		
+System.out.println("you pased ..."+args.length +"data");
+for(int i=0;i<args.length;i++) {
+	System.out.println("data is" +args[i]);
+}
+	}
+
+}
