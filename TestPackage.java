0e60889647db87347efea22299941e801dca298c
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 11:10:06 +0530

    Add files via upload

---
+++ TestPackage.java
@@ -0,0 +1,13 @@
+//import TN.Salem;
+public class TestPackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TN.Salem s=new TN.Salem();
+		s.whichpackage();
+		US.Salem s1=new US.Salem();
+		s1.whichpackage();
+
+	}
+
+}
