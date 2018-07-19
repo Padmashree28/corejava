dbd1ad2b5f67ddf22cc4140761de5702290f0c0b
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 14:56:42 +0530

    Add files via upload

---
+++ Employee3.java
@@ -0,0 +1,14 @@
+
+public class Employee3 {
+String name;
+Employee3(String stm){
+	name=stm;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee3 e=new Employee3("shree");
+System.out.println(e.name);
+
+	}
+
+}
