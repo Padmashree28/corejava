313b6cbe59f9c76f459a23190118f38c120e4081
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 12:43:03 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,19 @@
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int big=0;
+		int arr[]= {12,50,2,5,55,25};
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]>big) 
+	 big=arr[i];
+		}
+	 System.out.println("biggest number is:" +big);
+			}
+}
+
+	
+
+
+
