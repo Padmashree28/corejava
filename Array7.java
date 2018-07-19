8696da324ab677986c81caa7b53dc6523bf36b0a
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 12:49:28 +0530

    Add files via upload

---
+++ Array7.java
@@ -0,0 +1,17 @@
+
+public class Array7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String country[]= {"india","pakistan","china"};
+		String capital[]= {"delhi","xxxx","beijing"};
+		for(int i=0;i<country.length;i++) {
+			if(country[i].equals("china")) {
+				System.out.println("capital of " +country[i] + " " +"is:"+ " " +capital[i]);
+			}
+		}
+		
+
+	}
+
+}
