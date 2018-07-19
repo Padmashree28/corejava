54ea029d74b39c564dcd04c5ba74e95f1516e7df
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 19:17:16 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=1;i<=5;i++) // outer for loop - for rows
+		{
+			for (int j=1;j<=i;j++) // inner for loop
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+
+		}
+
+
+	}
+
+}
