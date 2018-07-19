e3388cb9669e4af08de7dfb821fc64f02304e66f
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 19:08:07 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,26 @@
+
+public class Biggest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10, b=60;
+		
+		if (a != 0 || b!=0)
+		{
+		if (a > b)
+			System.out.println(a+" is the biggest no");
+		else
+			if (b > a)
+			System.out.println(b+" is the biggest no");
+			else
+				System.out.println("Both are equal");
+		}
+		else
+		{
+			System.out.println("Both can't be zero");
+		}
+
+
+	}
+
+}
