70e39df7b7c9bb8824c01a0a1a5458d3253295b7
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-19 11:14:51 +0530

    Add files via upload

---
+++ MyTest.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import pack1.LabTrainer;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+LabTrainer l=new LabTrainer();
+l.a1();
+
+
+	}
+
+}
