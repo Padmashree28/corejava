eb77f834098222318416ecbe3408bd9469be89eb
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-23 10:34:07 +0530

    Add files via upload

---
+++ TestAbstract.java
@@ -0,0 +1,18 @@
+
+public class TestAbstract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pilot p=new pilot();
+		p.doyouspeak();
+		p.whatyoudo();
+		trainer t =new trainer();
+		t.doyouspeak();
+		t.whatyoudo();
+		doctor d=new doctor();
+		d.doyouspeak();
+		d.whatyoudo();
+
+	}
+
+}
