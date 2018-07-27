8b2daa6be40a7b4efad8393a2939713789d154f1
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-27 15:17:36 +0000

    Add files via upload

---
+++ EmployeeBo.java
@@ -0,0 +1,8 @@
+
+public class EmployeeBo {
+	public void calincomeTax(EmployeeVo evo)
+	{
+		double incomeTax= evo.getAnnualIncome()*0.3;
+		evo.setIncomeTax(incomeTax);
+	}
+}
---
+++ Testmain1.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class Testmain1 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int n;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter no of employees: ");
+		n=Integer.parseInt(br.readLine());
+		EmployeeVo ev[]=new EmployeeVo[n];
+		
+		for(int i=0;i<n;i++) {
+		
+		
+		System.out.println("Enter employee id: ");
+		int num=Integer.parseInt(br.readLine());
+		
+		System.out.println("Enter name: ");
+		String name=(br.readLine());
+		
+		System.out.println("Enter annual income: ");
+		double income =Double.parseDouble(br.readLine());
+		
+		ev[i]= new EmployeeVo(num, name, income);
+		}
+		
+		
+		// Let's call the method to calculate the it
+		EmployeeBo ebo=new EmployeeBo();
+		
+		for (int i=0;i<n;i++)
+		{
+			
+			
+			ebo.calincomeTax(ev[i]);
+			System.out.println(ev[i]);
+			
+		}
+		
+	}
+
+
+
+	}
+
+
