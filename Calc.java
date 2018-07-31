b04757520218f2551b40b0245a7c010ed1647336
Padmashree28 <41329833+Padmashree28@users.noreply.github.com>
2018-07-31 12:16:03 +0000

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,46 @@
+package com.deloitte;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calc
+ */
+@WebServlet("/Calc")
+public class Calc extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calc() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 PrintWriter pw=response.getWriter();
+	 int numb1=Integer.parseInt(request.getParameter("num1"));
+	 int numb2=Integer.parseInt(request.getParameter("num2"));
+	 int sum=numb1+numb2;
+	 pw.println("the sum is:"+sum);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- Employee.java
+++ Employee.java
@@ -1,34 +1,87 @@
+package com.deloitte;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Employee {
-private int emp_id;
-private String emp_name;
-private double emp_sal;
-static String companyname="Deloitte";
-static String getcompanyname() {
-	return companyname;
-}
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public Employee(int emp_id, String emp_name, double emp_sal) {
+/**
+ * Servlet implementation class Employee
+ */
+@WebServlet("/Employee")
+public class Employee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Employee() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	this.emp_id = emp_id;
-	this.emp_name = emp_name;
-	this.emp_sal = emp_sal;
-}
-
-public Employee() {
-	System.out.println("default constructor");
-	this.emp_id = 1;
-	this.emp_name = "james";
-	this.emp_sal = 11111.1;
 	
-}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw=response.getWriter();
+		double bonus=0.0,netsal=0.0;
+		String name=request.getParameter("empname");
+		int sal=Integer.parseInt(request.getParameter("sal"));
+		String grade=request.getParameter("grade");
+		int tax=25*sal/100;
+		
+		if(sal>100000 && grade.equals("A")) {
+		 bonus=5*10/100.0;
+			 netsal=sal+bonus-tax;
+		pw.println("The bonus is:" +bonus);
+		pw.println("The netsal is:" +netsal);
+		
+		}
+		else if(sal>50000 && sal<=100000 &&  grade.equals("B")) {
+			 bonus=7*10/100;
+			 netsal=sal+bonus-tax;
+			
+		pw.println("The bonus is:" +bonus);
+		pw.println("The netsal is:" +netsal);
+	}
+		else if(sal<50000 &&  grade.equals("C")) {
+			 bonus=20*10/100;
+			 netsal=sal+bonus-tax;
+		pw.println("The bonus is:" +bonus);
+		pw.println("The netsal is:" +netsal);
+	}
+		
+		pw.println("The tax is: " +tax);
+		//PrintWriter pw=response.getWriter();
+		String str1="<table border=1>"+
+		           "<tr><th>Empname</th>" +
+		           "<th>Sal</th>" +
+		           "<th>Bonus</th>" +
+		           "<th>Tax</th>" +
+		           "<th>Netsal</th>" +
+		           "</tr>";
+		           
+		String str2="<table border=1>"+
+		           "<td>"+name+"</td>" +
+		           "<td>"+sal+"</td>" +
+		           "<td>"+bonus+"</td>" +
+		           "<th>"+tax+"</td>" +
+		           "<td>"+netsal+"</td>" +
+		           "</table>";
+		pw.println(str1+str2);
+		           
+	}
+		
 
-public double calculatebonus() {
-	double bonus=0.0;
-	if(this.emp_sal <=5000)
-		bonus=this.emp_sal*.2;
-	else
-		bonus=this.emp_sal*.1;
-	return bonus;
-}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
 }
